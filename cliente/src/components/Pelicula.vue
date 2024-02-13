1485626b033b3dd4a22d5f9cb1272e765b120bd1
AlejandroMorellano <20213tn026@utez.edu.mx>
2024-02-13 14:53:31 -0600

    [GET][CHANGE-STATUS] Falta [UPDATE]

--- cliente/src/components/Pelicula.vue
+++ cliente/src/components/Pelicula.vue
@@ -7,24 +7,48 @@
 
     <b-col>
       <b-row>
-        <b-col cols="12" md="4" lg="3" v-for="a in 5" :key="a">
+        <b-col cols="12" md="4" lg="3" v-for="movie in movies" :key="movie.id">          
           <div>
             <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 20rem"
-              class="mb-2"
-            >
-              <b-card-text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </b-card-text>
-            </b-card>
+            :img-src="movie.image"
+            :title="movie.title"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2"
+            :items="movies"
+            :per-page="perPage"
+            :current-page="currentPage"
+            :fields="fields"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+            label-sort-asc=""
+            label-sort-desc=""
+            small
+          >
+          <b-card-text>Description: {{ movie.description }}</b-card-text>
+          <b-card-text>Director: {{ movie.director }}</b-card-text>
+          <b-card-text>Publish Date: {{ movie.publishDate }}</b-card-text>
+          <b-card-text>Category: {{ movie.category.name }}</b-card-text>
+          <b-card-text>Status: {{ movie.status }}</b-card-text>
+          <b-button @click="changeStatus(movie)">Change Status</b-button>
+
+        </b-card>
+          </div>
+          <div v-if="movies.length < 0"> 
+            <p>No hay películas disponibles.</p>
           </div>
         </b-col>
+        <div class="overflow-auto">
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+            aria-controls="my-table"
+          ></b-pagination>
+          <p class="mt-3">Current Page: {{ currentPage }}</p>
+        </div>
       </b-row>
     </b-col>
 
@@ -146,6 +170,13 @@ export default {
         },
       ],
       modalVisible: false,
+      filtro: null,
+      sortBy: "title",
+      sortDesc: false,
+      perPage: 5,
+      rows: 0,
+      currentPage: 5,
+      isUploading: false,
       title: "",
       description: "",
       director: "",
@@ -165,13 +196,38 @@ export default {
       fechaState: null,
       categoryState: null,
       status: 1,
-      movie: [],
+      movies: [],
+      fields: [
+        { key: "title", label: "description", sortable: true },
+        { key: "director", label: "publishDate", sortable: true },
+        { key: "image", label: "category", sortable: true },
+      ],
     };
   },
   mounted() {
     this.handleSubmit();
+    this.getMovie();
+    this.changeStatus();
   },
   methods: {
+    async getMovie() {
+      try {
+        const response = await PeliculaService.getMovie();
+        console.log("RESPUESTA:", response);
+
+        this.movies = response.data.content;
+        console.log("DATA:", this.movies);
+
+        if (this.movies.length === 0) {
+          console.log("No hay películas disponibles.");
+        } else {
+          console.error("EXITO");
+        }
+      } catch (error) {
+        console.log("ERROR GET:", error);
+      }
+    },
+
     checkFormValidity() {
       const valid = this.$refs.form.checkValidity();
       this.titleState = valid;
@@ -203,6 +259,14 @@ export default {
         console.log("Registro exitoso!", data);
       } catch (error) {
         console.error("Error al registrar la película:", error);
+      }
+    },
+    async changeStatus (movie) {
+      movie.status = !movie.status;
+      try {
+        await PeliculaService.changeStatus(movie.id, movie.status);
+      } catch (error) {
+        movie.status = !movie.status;
       }
     },
     resetForm() {
--- cliente/src/services/PeliculaServ.js
+++ cliente/src/services/PeliculaServ.js
@@ -3,23 +3,46 @@ import axios from "axios";
 let api = "http://localhost:8080/api/movie/";
 
 const saveMovie = async (movieData) => {
-    try {
-      const response = await axios.post(api, movieData, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      console.log("Registro exitoso!", response);
-      return response;
-    } catch (error) {
-      console.error("Error al enviar la solicitud:", error);
-      if (error.response) {
-        console.error("Respuesta del servidor:", error.response.data);
+  try {
+    const response = await axios.post(api, movieData, {
+      headers: {
+        'Content-Type': 'application/json'
       }
-      throw error;
+    });
+    console.log("Registro exitoso!", response);
+    return response;
+  } catch (error) {
+    console.error("Error al enviar la solicitud:", error);
+    if (error.response) {
+      console.error("Respuesta del servidor:", error.response.data);
     }
+    throw error;
+  }
 };
 
-export default {
-    saveMovie,
+const getMovie = async () => {
+  try {
+    const response = await axios.post(api + "paged/");
+    console.log("Get exitoso!", response);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const changeStatus = async (movieId, status) => {
+  try {
+    const response = await axios.patch(api + movieId , { status: status})
+    console.log("Change status!", response);
+    return response.data;
+  } catch (error) {
+    console.log("Change status fallo!", error);
+    throw error;
+  }
 }
+
+export default {
+  saveMovie,
+  getMovie,
+  changeStatus
+};
